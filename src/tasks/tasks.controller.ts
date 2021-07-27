4cebe59c995189378412d43f98a42c9641ec9797
Matheus Silveira <matheus.silveira@omegaenergia.com.br>
2021-07-27 17:06:20 -0300

    DTO e primeiros metodos

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,22 @@
+import { Body, Get, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService){}
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log(title, description);
+    return this.tasksService.createTask(title, description);
+  }
 }
